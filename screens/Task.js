16026ac562ec67d914fa169a183827e28b6816eb
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-10 13:14:53 +0300

    created home about and task.js

---
+++ screens/Task.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Task = () => {
+  return (
+    <View>
+      <Text>Task</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    
+});
+
+
+export default Task
